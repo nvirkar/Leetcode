a8d8d2ec90a0617bbe05f1ba52a9956062d284ba
nvirkar <ninadvirkar61@gmail.com>
2022-07-28 01:34:44 +0530

    Added plusOne.js

---
+++ plusOne.js
@@ -0,0 +1,25 @@
+/**
+ * Leetcode-66
+ * Plus One
+ */
+
+var plusOne = function (digits) {
+    digitsLength = digits.length
+    for (i = digitsLength - 1; i > -1; i--) {
+        if (digits[i] !== 9) {
+            digits[i]++;
+            return digits
+        } else {
+            digits[i] = 0
+        }
+    }
+    digits.unshift(1)
+    return digits
+};
+
+
+console.log(plusOne([1, 2, 3]))
+console.log(plusOne([4, 3, 2, 1]))
+console.log(plusOne([9]))
+console.log(plusOne([9, 9, 9]))
+
