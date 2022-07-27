f3c835664e0e621f38d0e6f5f63e5bc2f44a4e17
nvirkar <ninadvirkar61@gmail.com>
2022-07-28 02:23:31 +0530

    Added removeElement.js

---
+++ removeElement.js
@@ -0,0 +1,18 @@
+/**
+ * Leetcode-27
+ * Remove Element
+ */
+
+var removeElement = function (nums, val) {
+    pointer1 = 0;
+    for (pointer2 = 0; pointer2 < nums.length; pointer2++) {
+        if (nums[pointer2] !== val) {
+            nums[pointer1] = nums[pointer2];
+            pointer1++
+        }
+    }
+    return pointer1;
+};
+
+console.log(removeElement([3, 2, 2, 3], 3))
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2))
