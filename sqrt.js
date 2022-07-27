73b2a18be5985555f797ab883e0adc61fd473006
nvirkar <ninadvirkar61@gmail.com>
2022-07-28 01:27:42 +0530

    Added sqrt.js

---
+++ sqrt.js
@@ -0,0 +1,29 @@
+/**
+ * Leetcode-69
+ * Sqrt(x)
+ */
+
+
+var mySqrt = function (x) {
+    if (x == 1) {
+        return 1
+    }
+    low = 1;
+    high = x;
+    while (low <= high) {
+        mid = low + Math.floor((high - low) / 2)
+        if (mid * mid == x) {
+            return mid;
+        } else if (mid * mid > x) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return low - 1
+};
+
+
+console.log(mySqrt(4))
+console.log(mySqrt(8))
