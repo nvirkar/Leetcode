96b5cd24f87a2af66ed17158c892c4d48b4ffee3
nvirkar <ninadvirkar61@gmail.com>
2022-07-28 01:53:57 +0530

    Added validAnagram.js

---
+++ validAnagram.js
@@ -0,0 +1,37 @@
+/**
+ * Leetcode-242
+ * Valid Anagram
+ */
+
+
+var isAnagram = function (s, t) {
+    sLength = s.length;
+    tLength = t.length;
+    if (sLength !== tLength) {
+        return false
+    }
+    dict = {}
+    for (i = 0; i < sLength; i++) {
+        if (!(s[i] in dict)) {
+            dict[s[i]] = 1
+        } else {
+            dict[s[i]]++
+        }
+    }
+    for (i = 0; i < tLength; i++) {
+        if (!(t[i] in dict)) {
+            return false
+        }
+        if (dict[t[i]] == 0) {
+            return false
+        } else {
+            dict[t[i]]--;
+        }
+    }
+    return true
+};
+
+
+
+console.log(isAnagram("anagram", "nagaram"))
+console.log(isAnagram("rat", "car"))
