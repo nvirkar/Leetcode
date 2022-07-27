00979d6166a8a17079f900d387d7940bfa498f2c
nvirkar <ninadvirkar61@gmail.com>
2022-07-28 00:32:18 +0530

    Added pallindromeNumber.js

---
+++ pallindromeNumber.js
@@ -0,0 +1,23 @@
+/**
+ * Leetcode-9
+ * Palindrome Number
+ */
+
+var isPalindrome = function (x) {
+    if (x < 0) {
+        return false;
+    }
+    originalNumber = x;
+    reverse = 0;
+    while (x != 0) {
+        rem = x % 10;
+        reverse = reverse * 10 + rem;
+        x = Math.floor(x / 10)
+    }
+    return reverse === originalNumber
+};
+
+console.log(isPalindrome(123));
+console.log(isPalindrome(121));
+console.log(isPalindrome(-121));
+console.log(isPalindrome(10));
