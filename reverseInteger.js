e887280a487dc2810b69400fb02cc6332ffa219e
nvirkar <ninadvirkar61@gmail.com>
2022-08-01 01:43:58 +0530

    reverseInteger.js

---
+++ reverseInteger.js
@@ -0,0 +1,27 @@
+/**
+ * Leetcode-7
+ * Reverse Integer
+ */
+
+var reverse = function (x) {
+    result = 0;
+    sign = x < 0 ? -1 : 1
+    x = x * sign;
+    while (x != 0) {
+        rem = x % 10;
+        result = result * 10 + rem;
+        x = Math.floor(x / 10)
+    }
+    result = result * sign;
+    max = Math.pow(2, 31) - 1
+    min = Math.pow(2, 31) * (-1)
+    if (result >= max || result <= min) {
+        return 0
+    }
+    return result
+};
+
+console.log(reverse(123))
+console.log(reverse(-123))
+console.log(reverse(120))
+console.log(reverse(1534236469))
