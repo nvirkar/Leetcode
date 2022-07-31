ec918ea02b0c65016e99ff3b3f3e7bf58d3aa3f2
nvirkar <ninadvirkar61@gmail.com>
2022-08-01 01:22:59 +0530

    sum.js

---
+++ sum.js
@@ -0,0 +1,13 @@
+/**
+ * Leetcode-2235
+ * Add Two Integers
+ */
+
+
+var sum = function (num1, num2) {
+    res = num1 + num2
+    return res
+};
+
+console.log(sum(12, 5))
+console.log(sum(-10, 4))
