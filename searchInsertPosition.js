72c8c6bec586eb35757f04302997aa7d7e77ef44
nvirkar <ninadvirkar61@gmail.com>
2022-08-07 17:57:48 +0530

    searchInsertPosition.js

---
+++ searchInsertPosition.js
@@ -0,0 +1,31 @@
+/**
+ * Leetcode-35
+ * Search Insert Position
+ */
+
+var searchInsert = function (nums, target) {
+    low = 0;
+    numsLength = nums.length
+    high = numsLength - 1
+    if (target > nums[high]) {
+        return numsLength
+    }
+    if (target < nums[0]) {
+        return 0
+    }
+    while (low <= high) {
+        mid = low + Math.floor((high - low) / 2)
+        if (nums[mid] == target) {
+            return mid
+        } else if (nums[mid] < target) {
+            low = mid + 1
+        } else {
+            high = mid - 1
+        }
+    }
+    return low
+};
+
+console.log(searchInsert([1, 3, 5, 6], 5))
+console.log(searchInsert([1, 3, 5, 6], 7))
+console.log(searchInsert([1, 3, 5, 6], 2))
