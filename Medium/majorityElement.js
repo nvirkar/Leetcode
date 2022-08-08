c55df3a143629866a5dc6b9336af2de5d06d1456
nvirkar <ninadvirkar61@gmail.com>
2022-08-08 16:08:41 +0530

    majorityElement II

---
+++ Medium/majorityElement.js
@@ -0,0 +1,27 @@
+/**
+ * Leetcode-229
+ * Majority Element II
+ */
+
+var majorityElement = function (nums) {
+    numsLength = nums.length;
+    ceiling = Math.floor(numsLength / 3)
+    res = []
+    dict = {}
+    for (i = 0; i < numsLength; i++) {
+        if (!(nums[i] in dict)) {
+            dict[nums[i]] = 1
+        } else {
+            dict[nums[i]]++
+        }
+        if (dict[nums[i]] > ceiling && !(res.includes(nums[i]))) {
+            res.push(nums[i]);
+        }
+    }
+    return res;
+};
+
+console.log(majorityElement([3, 2, 3]))
+console.log(majorityElement([1]))
+console.log(majorityElement([1, 2]))
+
