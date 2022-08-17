86cad236b3c4d620d7e36c17e4413de13343b793
nvirkar <ninadvirkar61@gmail.com>
2022-08-18 01:39:57 +0530

    uniqueMorseRepresentation.js

---
+++ Easy/uniqueMorseRepresentations.js
@@ -0,0 +1,24 @@
+/**
+ * Leetcode-804
+ * Unique Morse Code Words
+ */
+
+
+var uniqueMorseRepresentations = function (words) {
+    res = []
+    morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
+    for (let i = 0; i < words.length; i++) {
+        morseWord = ""
+        for (let j = 0; j < words[i].length; j++) {
+            morseWord += morse[words[i].charCodeAt(j) - 97]
+        }
+        if (!res.includes(morseWord)) {
+            res.push(morseWord)
+        }
+    }
+    return res.length;
+};
+
+
+console.log(uniqueMorseRepresentations(["gin", "zen", "gig", "msg"]))
+console.log(uniqueMorseRepresentations(["a"]))
