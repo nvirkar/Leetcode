b6d6cf1aa0de17f686ac281ac0bd26fc2efed610
nvirkar <ninadvirkar61@gmail.com>
2022-10-08 03:05:25 +0530

    added binarySearch.js

---
+++ Easy/binarySearch.js
@@ -0,0 +1,26 @@
+/**
+ * Leetcode-704
+ * Binary Search
+ * https://leetcode.com/problems/binary-search/
+ */
+
+var search = function (nums, target) {
+    numsLength = nums.length;
+    low = 0;
+    high = numsLength - 1;
+    while (low <= high) {
+        mid = low + Math.floor((high - low) / 2);
+        if (nums[mid] == target) {
+            return mid
+        }
+        else if (nums[mid] > target) {
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
+    }
+    return -1
+};
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9))
+console.log(search([-1, 0, 3, 5, 9, 12], 2))
