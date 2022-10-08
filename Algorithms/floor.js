38e86f3bd3b2ab8ebaea3b7daf87e261110378f1
nvirkar <ninadvirkar61@gmail.com>
2022-10-09 01:59:20 +0530

    ceil & floor

---
+++ Algorithms/floor.js
@@ -0,0 +1,40 @@
+
+main = () => {
+    // sorted in ascending
+    nums1 = [2, 3, 5, 9, 14, 16, 18];
+
+    console.log(floor(nums1, 14))
+    console.log(floor(nums1, 15))
+    console.log(floor(nums1, 4))
+    console.log(floor(nums1, 9))
+    console.log(floor(nums1, 18))
+    console.log(floor(nums1, 27))
+    console.log(floor(nums1, 1))
+
+
+}
+
+
+floor = (nums, target) => {
+    low = 0;
+    high = nums.length - 1;
+    if (target < nums[low]) {
+        return -1
+    }
+    while (low <= high) {
+        mid = low + Math.floor((high - low) / 2)
+        if (nums[mid] == target) {
+            return mid
+        }
+
+        if (nums[mid] > target) {
+            high = mid - 1
+        }
+        else {
+            low = mid + 1
+        }
+    }
+    return high
+}
+
+main()
