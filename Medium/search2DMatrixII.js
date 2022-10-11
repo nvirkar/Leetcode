12e746bd2f640d17debe26967166ea4be2b328f1
nvirkar <ninadvirkar61@gmail.com>
2022-10-12 03:37:57 +0530

    search2DMatrixII.js

---
+++ Medium/search2DMatrixII.js
@@ -0,0 +1,37 @@
+/**
+ * Leetcode-240
+ * Search a 2D Matrix II
+ * https://leetcode.com/problems/search-a-2d-matrix-ii
+ */
+
+
+var searchMatrix = function (matrix, target) {
+    row = 0
+    col = matrix[0].length - 1
+
+    while (row < matrix.length && col >= 0) {
+        if (matrix[row][col] == target) {
+            return true
+        } else if (matrix[row][col] > target) {
+            col--
+        } else {
+            row++
+        }
+    }
+    return false
+};
+
+
+console.log(searchMatrix([
+    [1, 4, 7, 11, 15],
+    [2, 5, 8, 12, 19],
+    [3, 6, 9, 16, 22],
+    [10, 13, 14, 17, 24],
+    [18, 21, 23, 26, 30]], 5))
+
+console.log(searchMatrix([
+    [1, 4, 7, 11, 15],
+    [2, 5, 8, 12, 19],
+    [3, 6, 9, 16, 22],
+    [10, 13, 14, 17, 24],
+    [18, 21, 23, 26, 30]], 20))
