c42291a9294c42c9b60bb22c486f87c4565325be
nvirkar <ninadvirkar61@gmail.com>
2022-10-13 00:25:14 +0530

    optimised

--- Algorithms/bubbleSort.js
+++ Algorithms/bubbleSort.js
@@ -4,24 +4,43 @@
  * Exchange Sort
  */
 
+/**
+ * space complexity O(1) 
+ * No extra space is needed as n increases
+ * Its an inplace sorting algorithm
+ * 
+ * Time complexity
+ * Best case O(n) => its already sorted
+ * Worst case O(n^2) => sorted in descing order
+ */
+
 main = function () {
     arr = [3, 1, 5, 4, 2]
+    // arr = [1, 2, 3, 4, 5]
     console.log("unsorted", arr)
     bubbleSort(arr)
-    console.log("sorted", arr)
+    console.log("sorted  ", arr)
 }
 
 bubbleSort = function (arr) {
+    swapped = false;
+    // i goes from 0 to n-1
     for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length - 1 - i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = temp
+        swapped = false;
+        // j goes from 1 to n-i
+        for (let j = 0; j < arr.length - i; j++) {
+            if (arr[j - 1] > arr[j]) {
+                temp = arr[j - 1]
+                arr[j - 1] = arr[j]
+                arr[j] = temp
+                swapped = true
             }
+        }
+        if (swapped == false) {
+            break
         }
     }
 }
 
-
 main()
+
