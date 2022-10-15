c08a05106f6990c69306ef5818c481c759a93454
nvirkar <ninadvirkar61@gmail.com>
2022-10-16 01:47:03 +0530

    cyclic sort logic

--- Easy/missingNumber.js
+++ Easy/missingNumber.js
@@ -4,15 +4,43 @@
  * https://leetcode.com/problems/missing-number/
  */
 
+// var missingNumber = function (nums) {
+//     sum = 0;
+//     numsLength = nums.length;
+//     for (i = 0; i < numsLength; i++) {
+//         sum += nums[i]
+//     }
+//     calculatedSum = (numsLength * (numsLength + 1)) / 2;
+//     return calculatedSum - sum;
+// };
+
+
+swap = function (nums, first, second) {
+    temp = nums[first]
+    nums[first] = nums[second]
+    nums[second] = temp
+}
 var missingNumber = function (nums) {
-    sum = 0;
-    numsLength = nums.length;
-    for (i = 0; i < numsLength; i++) {
-        sum += nums[i]
+    i = 0;
+    while (i < nums.length) {
+        correct = nums[i]
+        if (nums[i] < nums.length && nums[correct] != nums[i]) {
+            swap(nums, correct, i)
+        } else {
+            i++
+        }
     }
-    calculatedSum = (numsLength * (numsLength + 1)) / 2;
-    return calculatedSum - sum;
+    for (i = 0; i < nums.length; i++) {
+        if (nums[i] != i) {
+            return i
+        }
+    }
+    return nums.length
 };
+
+
+
+
 
 console.log(missingNumber([3, 0, 1]))
 console.log(missingNumber([0, 1]))
