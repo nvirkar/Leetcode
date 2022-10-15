cc1f18954e72310b2d5f563208d702d576d677ca
nvirkar <ninadvirkar61@gmail.com>
2022-10-16 02:18:11 +0530

    findDisappearedNumbers.js

---
+++ Easy/findDisappearedNumbers.js
@@ -0,0 +1,35 @@
+/**
+ * Leetcode-448
+ * Find All Numbers Disappeared in an Array
+ * https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+ */
+
+
+swap = function (nums, first, last) {
+    temp = nums[first]
+    nums[first] = nums[last]
+    nums[last] = temp
+}
+
+var findDisappearedNumbers = function (nums) {
+    res = []
+    i = 0;
+    while (i < nums.length) {
+        correct = nums[i] - 1
+        if (nums[i] != nums[correct]) {
+            swap(nums, i, correct)
+        } else {
+            i++
+        }
+    }
+    for (i = 0; i < nums.length; i++) {
+        if (nums[i] != i + 1) {
+            res.push(i + 1)
+        }
+    }
+    return res
+};
+
+
+console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]))
+console.log(findDisappearedNumbers([1, 1]))
