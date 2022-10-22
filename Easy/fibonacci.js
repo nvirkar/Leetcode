6fb52c5088c0eade8ee1be168e3473ed2b0b70f8
nvirkar <ninadvirkar61@gmail.com>
2022-10-23 00:56:59 +0530

    optimised

--- Easy/fibonacci.js
+++ Easy/fibonacci.js
@@ -4,30 +4,12 @@
  * https://leetcode.com/problems/fibonacci-number/
  */
 
-var getFib = function (n) {
-    if (n < 2) {
-        return arr[n]
-    } else {
-        if (arr[n - 1]) {
-            first = arr[n - 1]
-        } else {
-            first = getFib(n - 1)
-            arr[n - 1] = first
-        }
-        if (arr[n - 2]) {
-            second = arr[n - 2]
-        } else {
-            second = getFib(n - 2)
-            arr[n - 2] = second
-        }
-        return first + second
-    }
-}
-
 var fib = function (n) {
-    arr = [0, 1]
-    ans = getFib(n)
-    return ans
+    arr = [0, 1, 1]
+    for (i = 3; i <= n; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2]
+    }
+    return arr[n]
 };
 
 console.log(fib(0))
