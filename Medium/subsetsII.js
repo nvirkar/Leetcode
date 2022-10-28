a45e30bdbdaaad967e9032db3c3171911d5d6c35
nvirkar <ninadvirkar61@gmail.com>
2022-10-28 21:20:31 +0530

    iterative approach

--- Medium/subsetsII.js
+++ Medium/subsetsII.js
@@ -4,12 +4,36 @@
  * https://leetcode.com/problems/subsets-ii/
  */
 
+
+// iterative
 var subsetsWithDup = function (nums) {
     nums.sort()
-    res = []
-    getSubsets(nums, [])
-    return [...new Set(res.map(JSON.stringify))].map(JSON.parse)
+    let res = [[]]
+    start = 0;
+    end = 0;
+    for (i = 0; i < nums.length; i++) {
+        start = 0;
+        if (i > 0 && nums[i] == nums[i - 1]) {
+            start = end + 1;
+        }
+        end = res.length - 1
+        let newRes = JSON.parse(JSON.stringify(res)).slice(start)
+        for (j = 0; j < newRes.length; j++) {
+            newRes[j].push(nums[i]);
+        }
+        res.push(...newRes)
+    }
+    return res
 };
+
+
+// recursive
+// var subsetsWithDup = function (nums) {
+//     nums.sort()
+//     res = []
+//     getSubsets(nums, [])
+//     return [...new Set(res.map(JSON.stringify))].map(JSON.parse)
+// };
 
 var getSubsets = function (processed, unprocessed) {
     if (processed.length == 0) {
