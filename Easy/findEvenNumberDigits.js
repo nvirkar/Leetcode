5e834de81b72755984831ccfcae1a2823948aed5
nvirkar <ninadvirkar61@gmail.com>
2023-06-15 02:16:30 +0530

    findEvenNumberDigits.js

--- Easy/findEvenNumberDigits.js
+++ Easy/findEvenNumberDigits.js
@@ -2,19 +2,36 @@
  * Leetcode-1295
  * Find Numbers with Even Number of Digits
  * https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+ * #LinearSearch
  */
 
-
 var findNumbers = function (nums) {
-    count = 0;
-    for (let i = 0; i < nums.length; i++) {
-        numLength = nums[i].toString().length;
-        if (numLength % 2 == 0) {
-            count++;
-        }
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let numLength = nums[i].toString().length;
+    if (numLength % 2 == 0) {
+      count++;
     }
-    return count;
+  }
+  return count;
 };
 
-console.log(findNumbers([12, 345, 2, 6, 7896]))
-console.log(findNumbers([555, 901, 482, 1771]))
+// Method 2 count no of digits using log or while loop
+
+// var even = function (num) {
+//   let digitsLength = Math.floor(Math.log10(num) + 1);
+//   return digitsLength % 2 == 0;
+// };
+
+// var findNumbers = function (nums) {
+//   let count = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (even(nums[i])) {
+//       count++;
+//     }
+//   }
+//   return count;
+// };
+
+console.log(findNumbers([12, 345, 2, 6, 7896]));
+console.log(findNumbers([555, 901, 482, 1771]));
