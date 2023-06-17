977d573b2957908c01a2b3025e105c1282315d85
nvirkar <ninadvirkar61@gmail.com>
2023-06-18 04:25:31 +0530

    nextGreatestLetter

--- Easy/nextGreatestLetter.js
+++ Easy/nextGreatestLetter.js
@@ -2,21 +2,24 @@
  * Leetcode-744
  * Find Smallest Letter Greater Than Target
  * https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+ * #BinarySearch
  */
 
 var nextGreatestLetter = function (letters, target) {
-    low = 0
+    low = 0;
     high = letters.length - 1;
+
     if (target >= letters[high]) {
-        return letters[0]
+        return letters[0];
     }
+
     while (low <= high) {
         mid = low + Math.floor((high - low) / 2);
 
         if (target < letters[mid]) {
-            high = mid - 1
+            high = mid - 1;
         } else {
-            low = mid + 1
+            low = mid + 1;
         }
     }
     return letters[low];
