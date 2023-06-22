69b3f0a1baf1ef055213785cc1d89aa760f496c5
nvirkar <ninadvirkar61@gmail.com>
2023-06-23 02:48:29 +0530

    validPerfectSquare

--- Easy/validPerfectSquare.js
+++ Easy/validPerfectSquare.js
@@ -2,26 +2,28 @@
  * Leetcode-367
  * Valid Perfect Square
  * https://leetcode.com/problems/valid-perfect-square/
+ * #BinarySearch
  */
 
 var isPerfectSquare = function (num) {
     if (num == 1) {
-        return true
+        return true;
     }
-    low = 1;
+
+    low = 2;
     high = num;
+
     while (low <= high) {
         mid = low + Math.floor((high - low) / 2)
         if (mid * mid == num) {
             return true;
-        } else if (mid * mid > num) {
-            high = mid - 1
-        }
-        else {
-            low = mid + 1
+        } else if (mid * mid < num) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
         }
     }
-    return false
+    return false;
 };
 
 console.log(isPerfectSquare(16))
