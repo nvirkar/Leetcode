0e11d46f457f957f0bd2b7aa4860507cb2611032
nvirkar <ninadvirkar61@gmail.com>
2023-06-23 03:02:47 +0530

    guessNumber

--- Easy/guessNumber.js
+++ Easy/guessNumber.js
@@ -2,20 +2,21 @@
  * Leetcode-374
  * Guess Number Higher or Lower
  * https://leetcode.com/problems/guess-number-higher-or-lower/
+ * #BinarySearch
  */
 
 var guessNumber = function (n) {
-    low = 0;
+    low = 1;
     high = n;
 
     while (low <= high) {
         mid = low + Math.floor((high - low) / 2)
         if (guess(mid) == 0) {
-            return mid
-        } else if (guess(mid) > 0) {
-            low = mid + 1
+            return mid;
+        } else if (guess(mid) == -1) {
+            high = mid - 1;
         } else {
-            high = mid - 1
+            low = mid + 1;
         }
     }
 };
