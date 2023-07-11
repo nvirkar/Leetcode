ef75c443350b91b013fb3321456834429aad104f
nvirkar <ninadvirkar61@gmail.com>
2023-07-12 02:39:35 +0530

    searchSortedMatrix

--- Medium/searchSortedMatrix.js
+++ Medium/searchSortedMatrix.js
@@ -2,58 +2,63 @@
  * Leetcode-74
  * Search a 2D Matrix
  * https://leetcode.com/problems/search-a-2d-matrix/
+ * #BinarySearch
  */
 
 
 // search in row between columns provided
 var binarySearch = function (matrix, row, colStart, colEnd, target) {
+
     while (colStart <= colEnd) {
         mid = colStart + Math.floor((colEnd - colStart) / 2)
+
         if (matrix[row][mid] == target) {
             return true
-        } else if (matrix[row][mid] < target) {
-            colStart = mid + 1
-        } else {
+        } else if (matrix[row][mid] > target) {
             colEnd = mid - 1
+        } else {
+            colStart = mid + 1
         }
     }
     return false
 }
 
+var getRow = function (matrix, target) {
+    // get floor from first colum to identify row present
+    col = 0;
+    rStart = 0;
+    rEnd = matrix.length - 1
+
+    if (target <= matrix[0][0]) {
+        return 0
+    }
+
+    while (rStart <= rEnd) {
+        mid = rStart + Math.floor((rEnd - rStart) / 2)
+        if (matrix[mid][col] == target) {
+            return mid
+        } else if (matrix[mid][col] > target) {
+            rEnd = mid - 1
+        } else {
+            rStart = mid + 1
+        }
+    }
+    return rEnd
+}
 
 var searchMatrix = function (matrix, target) {
-    row = matrix.length;
-    col = matrix[0].length;
-
-    if (row == 1) {
-        return binarySearch(matrix, 0, 0, col - 1, target)
-    }
-
-    rStart = 0
-    rEnd = row - 1
-
-    cMid = Math.floor(col / 2)
-    //run loop till 2 rows are remaining
-    while (rStart < rEnd - 1) {
-        mid = rStart + Math.floor((rEnd - rStart) / 2)
-        if (matrix[mid][cMid] == target) {
-            return true
-        }
-        else if (matrix[mid][cMid] < target) {
-            rStart = mid
-        } else {
-            rEnd = mid
-        }
-    }
-
-    first = binarySearch(matrix, rStart, 0, col - 1, target)
-    if (first == false) {
-        return binarySearch(matrix, rEnd, 0, col - 1, target)
-    } else {
-        return first
-    }
+    row = getRow(matrix, target)
+    return binarySearch(matrix, row, 0, matrix[0].length - 1, target)
 };
 
+console.log(
+    searchMatrix([[1]], 0))
+
+console.log(
+    searchMatrix(
+        [[1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]], 8))
 
 console.log(
     searchMatrix(
@@ -80,5 +85,8 @@ console.log(
         [10, 11, 16, 20],
         [23, 30, 34, 60]], 13))
 
-console.log(searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]],
-    13))
+console.log(
+    searchMatrix([
+        [1, 3, 5, 7],
+        [10, 11, 16, 20],
+        [23, 30, 34, 60]], 34))
