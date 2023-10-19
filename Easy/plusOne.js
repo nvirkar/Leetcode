758a8e7350174060c8f121621c1fb8e884a130a2
nvirkar <ninadvirkar61@gmail.com>
2023-10-20 02:33:14 +0530

    plusOne

--- Easy/plusOne.js
+++ Easy/plusOne.js
@@ -2,25 +2,25 @@
  * Leetcode-66
  * Plus One
  * https://leetcode.com/problems/plus-one/
+ * Math
  */
-
 var plusOne = function (digits) {
-    digitsLength = digits.length
-    for (i = digitsLength - 1; i > -1; i--) {
-        if (digits[i] !== 9) {
-            digits[i]++;
-            return digits
-        } else {
-            digits[i] = 0
-        }
+  let n = digits.length;
+
+  for (i = n - 1; i >= 0; i--) {
+    if (digits[i] !== 9) {
+      digits[i]++;
+      return digits;
+    } else {
+      digits[i] = 0;
     }
-    digits.unshift(1)
-    return digits
+  }
+  digits.unshift(1);
+
+  return digits;
 };
 
-
-console.log(plusOne([1, 2, 3]))
-console.log(plusOne([4, 3, 2, 1]))
-console.log(plusOne([9]))
-console.log(plusOne([9, 9, 9]))
-
+console.log(plusOne([1, 2, 3]));
+console.log(plusOne([4, 3, 2, 1]));
+console.log(plusOne([9]));
+console.log(plusOne([9, 9, 9]));
