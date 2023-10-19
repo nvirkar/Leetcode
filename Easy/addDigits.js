eb769c7e6e43ddc815852d20643addc34bbf0aba
nvirkar <ninadvirkar61@gmail.com>
2023-10-20 02:40:25 +0530

    addDigits

--- Easy/addDigits.js
+++ Easy/addDigits.js
@@ -2,22 +2,22 @@
  * Leetcode-258
  * Add Digits
  * https://leetcode.com/problems/add-digits/
+ * Math
  */
 
-
 var addDigits = function (num) {
-    if (num < 10) {
-        return num
-    }
+  if (num <= 9) {
+    return num;
+  } else {
     sum = 0;
     while (num != 0) {
-        rem = num % 10;
-        sum = sum + rem;
-        num = Math.floor(num / 10)
+      rem = num % 10;
+      sum = sum + rem;
+      num = Math.floor(num / 10);
     }
-    return addDigits(sum)
+    return addDigits(sum);
+  }
 };
 
-
-console.log(addDigits(38))
-console.log(addDigits(0))
+console.log(addDigits(38));
+console.log(addDigits(0));
