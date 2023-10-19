def9d9b285c3fcca0fd8a0133ff3c25f8334de06
nvirkar <ninadvirkar61@gmail.com>
2023-10-20 02:22:35 +0530

    pallindromeNumber

--- Easy/pallindromeNumber.js
+++ Easy/pallindromeNumber.js
@@ -2,20 +2,21 @@
  * Leetcode-9
  * Palindrome Number
  * https://leetcode.com/problems/palindrome-number/
+ * Math
  */
 
 var isPalindrome = function (x) {
-    if (x < 0) {
-        return false;
-    }
-    originalNumber = x;
-    reverse = 0;
-    while (x != 0) {
-        rem = x % 10;
-        reverse = reverse * 10 + rem;
-        x = Math.floor(x / 10)
-    }
-    return reverse === originalNumber
+  if (x < 0) {
+    return false;
+  }
+  originalNumber = x;
+  reverse = 0;
+  while (x != 0) {
+    rem = x % 10;
+    reverse = reverse * 10 + rem;
+    x = Math.floor(x / 10);
+  }
+  return reverse === originalNumber;
 };
 
 console.log(isPalindrome(123));
