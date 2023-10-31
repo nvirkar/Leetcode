24f7460320e887bfbbec518909c96efe2a3fe4d1
nvirkar <ninadvirkar61@gmail.com>
2023-11-01 02:35:10 +0530

    numberOfSteps

--- Easy/numberOfSteps.js
+++ Easy/numberOfSteps.js
@@ -2,27 +2,25 @@
  *  Leetcode-1342
  * Number of Steps to Reduce a Number to Zero
  * https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+ * Recursion
  */
 
-
-var helper = function (num, count) {
-    if (num == 0) {
-        return count
-    }
-    if (num % 2 == 0) {
-        return helper(Math.floor(num / 2), count + 1)
-    } else {
-        return helper(num - 1, count + 1)
-    }
-}
-
-
 var numberOfSteps = function (num) {
-    return helper(num, 0)
+  return helper(num, 0);
 };
 
-console.log(numberOfSteps(14))
-console.log(numberOfSteps(8))
-console.log(numberOfSteps(123))
+var helper = function (num, steps) {
+  if (num == 0) {
+    return steps;
+  }
+  isEven = num % 2 == 0;
 
+  if (isEven) {
+    return helper(Math.floor(num / 2), steps + 1);
+  }
+  return helper(num - 1, steps + 1);
+};
 
+console.log(numberOfSteps(14));
+console.log(numberOfSteps(8));
+console.log(numberOfSteps(123));
