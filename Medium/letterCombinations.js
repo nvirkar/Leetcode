f31a676d5eaf29d6d7d49818bfbdd893232a4e59
nvirkar <ninadvirkar61@gmail.com>
2026-04-22 18:20:24 +0530

    updated tag

--- Medium/letterCombinations.js
+++ Medium/letterCombinations.js
@@ -2,29 +2,40 @@
  * Leetcode-17
  * Letter Combinations of a Phone Number
  * https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+ * #Recursion
  */
 
+getLetterCombinations = function (up, p) {
+  if (p == "") {
+    res.push(up);
+    return;
+  }
 
-getLetterCombinations = function (p, up) {
-    if (up == "") {
-        res.push(p)
-        return
-    }
-    let digit = parseInt(up.charAt(0))
-    let data = charArray[digit]
-    for (let i = 0; i < data.length; i++) {
-        getLetterCombinations(p + data[i], up.substring(1))
-    }
-}
+  let first = dict[p.charAt(0)];
 
-var letterCombinations = function (digits) {
-    charArray = { 2: "abc", 3: "def", 4: "ghi", 5: "jkl", 6: "mno", 7: "pqrs", 8: "tuv", 9: "wxyz" }
-    res = []
-    getLetterCombinations("", digits);
-    res = res.filter((el) => el != "")
-    return res;
+  for (let i = 0; i < first.length; i++) {
+    getLetterCombinations(up + first[i], p.substring(1));
+  }
 };
 
-console.log(letterCombinations("23"))
-console.log(letterCombinations(""))
-console.log(letterCombinations("2"))
+letterCombinations = function (digits) {
+  res = [];
+
+  dict = {
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  };
+
+  getLetterCombinations("", digits);
+  return res;
+};
+
+console.log(letterCombinations("23"));
+console.log(letterCombinations(""));
+console.log(letterCombinations("2"));
