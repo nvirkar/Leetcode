c864b2682ddca99f71220f4eea5f56402e4f5923
nvirkar <ninadvirkar61@gmail.com>
2026-05-01 05:19:58 +0530

    recursion

--- Easy/minMaxGame.js
+++ Easy/minMaxGame.js
@@ -2,21 +2,24 @@
  * Leetcode-2293
  * Min Max Game
  * https://leetcode.com/problems/min-max-game/
+ * #Recursion
  */
 
 var minMaxGame = function (nums) {
-    n = nums.length
-    if (n == 1) {
-        return nums[0]
-    }
-    let flag = true;
-    let newArr = []
-    for (i = 0; i < n; i += 2) {
-        let el = flag ? Math.min(nums[i], nums[i + 1]) : Math.max(nums[i], nums[i + 1])
-        newArr.push(el)
-        flag = !flag
-    }
-    return minMaxGame(newArr)
+  let n = nums.length;
+
+  if (n == 1) {
+    return nums[0];
+  }
+  let newNums = [];
+  for (let i = 0; i < Math.floor(n / 2); i++) {
+    newNums[i] =
+      i % 2 == 0
+        ? Math.min(nums[2 * i], nums[2 * i + 1])
+        : Math.max(nums[2 * i], nums[2 * i + 1]);
+  }
+
+  return minMaxGame(newNums);
 };
 
 console.log(minMaxGame([1, 3, 5, 2, 4, 8, 2, 2]));
